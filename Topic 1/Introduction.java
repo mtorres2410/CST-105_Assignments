0e0d7cf9a93efbfc5b11107bc9f36a122f5477c5
mtorres2410 <44320641+mtorres2410@users.noreply.github.com>
2018-10-21 16:56:49 -0400

    Add files via upload

--- Topic 1/Introduction.java
+++ Topic 1/Introduction.java
@@ -4,12 +4,12 @@
 public class Introduction {
 
     public static void main(String[] args) {
-        //Print the words My name is Miguel
-        System.out.println("My name is Miguel.");
-        //Print the words My favorite color is green.
-        System.out.println("My favorite color is green.");
-        //Print the words If I won the lottery, I would travel the world.
-        System.out.println("If I won the lottery, I would travel the world.");
+        //Print the words My name is Miguel on the screen
+        System.out.println("My name is Miguel."); 
+        //Print the words My favorite color is green 
+        System.out.println("My favorite color is green."); 
+        //Print the words If I won the lotter, I would travel the world.
+        System.out.println("If I won the lottery, I would travel the world."); 
     }
 
 }
