8e8c9492c90c2599a5f0f65ed2c0b1f8a2cf9956
DESKTOP-SGUQK1J\MiguelVR <migueltorresperez@outlook.com>
2018-10-21 16:36:09 -0400

    CST 105 Assignment 1

---
+++ Topic 1/Introduction.java
@@ -0,0 +1,12 @@
+//Miguel Torres Perez
+//CST-105 Programming Exercise 1
+
+public class Introduction {
+
+    public static void main(String[] args) {
+        System.out.println("My name is Miguel.");
+        System.out.println("My favorite color is green.");
+        System.out.println("If I won the lottery, I would travel the world.");
+    }
+
+}
