99065a087d4d7faf0f13e39620bb56877cec003f
mtorres2410 <44320641+mtorres2410@users.noreply.github.com>
2018-10-21 16:46:54 -0400

    Update Introduction.java

--- Topic 1/Introduction.java
+++ Topic 1/Introduction.java
@@ -4,8 +4,11 @@
 public class Introduction {
 
     public static void main(String[] args) {
+        //Print the words My name is Miguel
         System.out.println("My name is Miguel.");
+        //Print the words My favorite color is green.
         System.out.println("My favorite color is green.");
+        //Print the words If I won the lottery, I would travel the world.
         System.out.println("If I won the lottery, I would travel the world.");
     }
 
